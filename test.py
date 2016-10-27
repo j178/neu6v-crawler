da40137fff2afdb3631a0d32407dfe7035ef082a
John Jiang <nigelchiang@outlook.com>
2016-10-27 19:13:59 +0800

    load cookie from file

--- test.py
+++ test.py
@@ -1,24 +1,30 @@
+import json
+
 import requests
-from http.cookiejar import LWPCookieJar
+
+# from http.cookiejar import LWPCookieJar
 
 WATER_ZONE_URL = 'http://bt.neu6.edu.cn/forum-4-1.html'
+COOKIE_FILE = 'cookies-my.json'
+
+
+def load_cookies():
+    with open(COOKIE_FILE) as f:
+        try:
+            return json.load(f)
+        except json.decoder.JSONDecodeError as e:
+            raise SystemExit('Cookie file parse failed: ' + e.msg)
+
 
 s = requests.Session()
 
 # s.cookies = LWPCookieJar('cookies.txt')
 # s.cookies.load(ignore_discard=True, ignore_expires=True)
 
-s.headers[
-    'User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 ' \
-                    'Safari/537.36'
+s.headers['User-Agent'] = ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                           'Chrome/54.0.2840.71 Safari/537.36')
 
-cookies = {
-    'LRpW_2132_auth'   :
-        '63b8VSiDLrL0t2V%2FR%2FM4zQU%2BAzOpj%2B2qNShWIhMXqEE57qhTgufHwtzHiz2kEayG453zn740I4WJpwx8vLonwVZocDk',
-    'LRpW_2132_saltkey': 'GJJkJo4X',
-}
-
-s.cookies.update(cookies)
+s.cookies.update(load_cookies())
 
 if __name__ == '__main__':
 
@@ -26,7 +32,8 @@ if __name__ == '__main__':
 
     # s.cookies.save(ignore_discard=True, ignore_expires=True)
 
-    print(s.cookies)
-
     if '开心灌水' in rsp.text:
         print('登录成功')
+
+    with open('index.html', 'w') as f:
+        f.write(rsp.text)
